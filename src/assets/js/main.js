36acefec6a447bfd21b59bce3a394a528347afec
Mkrtich <amv.mkrtich@gmail.com>
2024-09-13 12:36:04 +0400

    Modify scroll effect components and optimize scroll behavior
    Restored `scroll-snap-type` and added background color styling for the `.scroll-effect` class. Introduced a new `cloneText` function for repeating text elements and enhanced smooth scroll behavior across sections. Removed redundant background color definitions and decluttered section HTML structure.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,14 +14,17 @@ function toggleClassOnScroll(className){
 
         const topElementToTopViewport = el.getBoundingClientRect().top;
 
-        if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 1){
+        if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight){
             el.classList.add('is-visible');
         }
     });
 }
 
-toggleClassOnScroll('animate-elem');
+toggleClassOnScroll('section-bg-text');
 
+window.addEventListener('scroll', function (){
+    toggleClassOnScroll('section-bg-text');
+});
 
 function closeMenu(){
     document.body.classList.remove('mobile-menu-open');
@@ -54,26 +57,79 @@ const phone = document.getElementById('inputPhone');
 
 if(phone) {
     phone.addEventListener('input', function(e) {
-        let value = e.target.value.replace(/\D/g, ''); // Remove all non-numeric characters
-        value = value.substring(0, 10); // Ensure max of 10 digits
+        phone.addEventListener('input', function(e) {
+            let value = e.target.value.replace(/\D/g, ''); // Remove all non-numeric characters
+            value = value.substring(0, 10); // Ensure max of 10 digits
 
-        // Apply mask
-        if (value.length > 0) {
-            value = '+7 ' + value;
-            if (value.length > 6) {
-                value = value.slice(0, 6) + '-' + value.slice(6);
+            // Apply mask
+            if (value.length > 0) {
+                value = '+7 ' + value;
+                if (value.length > 6) {
+                    value = value.slice(0, 6) + '-' + value.slice(6);
+                }
+                if (value.length > 10) {
+                    value = value.slice(0, 10) + '-' + value.slice(10);
+                }
+                if (value.length > 13) {
+                    value = value.slice(0, 13) + '-' + value.slice(13);
+                }
             }
-            if (value.length > 10) {
-                value = value.slice(0, 10) + '-' + value.slice(10);
-            }
-            if (value.length > 13) {
-                value = value.slice(0, 13) + '-' + value.slice(13);
-            }
-        }
 
-        e.target.value = value;
+            e.target.value = value;
+        });
     });
 }
+
+// clone text
+function cloneText(text){
+    const elements = document.querySelectorAll('.text-repeat');
+    if(!elements?.length) return;
+
+    elements.forEach(element => {
+        let content = '';
+        const count = element.dataset.count ? element.dataset.count : 5;
+        for(let i = 0; i < count; i++){
+            content += `<span>${text}</span>`
+        }
+        element.innerHTML = content;
+    })
+}
+cloneText("Видео");
+
+document.addEventListener('wheel', (event) => {
+    const container = document.querySelector('.scroll-effect');
+    const sections = container.querySelectorAll('.section');
+    const sectionHeight = sections[0].offsetHeight;
+    const currentScroll = container.scrollTop;
+    const top = container.querySelector("#top");
+    let targetSection;
+    const rect = container.getBoundingClientRect();
+    const topRect = top.getBoundingClientRect();
+    console.log(currentScroll);
+
+    if(rect.top < 0){
+        document.body.classList.add("enable-scroll-effect");
+        if (event.deltaY > 0) {
+            targetSection = Math.min(sections.length - 1, Math.floor(currentScroll / sectionHeight) + 1);
+        } else {
+            targetSection = Math.max(0, Math.floor(currentScroll / sectionHeight) - 1);
+        }
+        console.log(targetSection)
+        container.scrollTo({
+            top: targetSection * sectionHeight,
+            behavior: 'smooth'
+        });
+
+        if(currentScroll > 0){
+            cloneText("Велосипед")
+
+        }
+
+        // event.preventDefault();
+    }else{
+        document.body.classList.remove("enable-scroll-effect");
+    }
+});
 
 
 // alert(`Your screen resolution is: ${window.innerWidth} X ${window.innerHeight}`)
